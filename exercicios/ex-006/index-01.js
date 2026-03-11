7f824edaeff800a5566e0c9ce3e0f9f888ca2e45
guimovie <gduartedasilva3@gmail.com>
2026-03-11 19:16:33 -0300

    .

--- exercicios/ex-006/index-01.js
+++ exercicios/ex-006/index-01.js
@@ -1,7 +1,7 @@
-var age = 17
+var age = 100
 if (age < 16) {
     console.log('Não pode votar')
-} else if (age >= 16 && age <= 17) {
+} else if (age >= 16 && age <= 17 || age > 65) {
     console.log('voto opcional')
 } else {
     console.log('voto obrigatorio')
