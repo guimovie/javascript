b758d9f7f9b7f33445efa854ee83aa7f6667c70d
guimovie <gduartedasilva3@gmail.com>
2026-03-10 21:45:25 -0300

    Create index.js

---
+++ exercicios/ex-006/index.js
@@ -0,0 +1,8 @@
+var age = 17
+if (age < 16) {
+    console.log('Não pode votar')
+} else if (age >= 16 && age <= 17) {
+    console.log('voto opcional')
+} else {
+    console.log('voto obrigatorio')
+}
