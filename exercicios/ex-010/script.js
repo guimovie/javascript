649a69486b2840c6a4e23f4b8739441adacad22b
guimovie <gduartedasilva3@gmail.com>
2026-03-23 19:35:16 -0300

    Update script.js

--- exercicios/ex-010/script.js
+++ exercicios/ex-010/script.js
@@ -1,20 +1,32 @@
 function contar() {
-    var inicio = window.document.getElementById('inicio')
-    var fim = window.document.getElementById('fim')
-    var passo = window.document.getElementById('passo')
-    var iNumero = Number(inicio.value)
-    var fNumero = Number(fim.value)
-    var res = window.document.getElementById('res')
-    if (iNumero > fNumero) {
-        window.alert(`Insira uma contagem válida!`)
-    } else {
-        res.innerHTML = ' '
-        while (iNumero <= fNumero) {
-            res.innerHTML += iNumero
-            res.innerHTML += ' 👉 '
-            iNumero = iNumero + Number(passo.value)
-        }
-        res.innerHTML += ' ✅'
-    }
+    let inicio = window.document.getElementById('inicio')
+    let fim = window.document.getElementById('fim')
+    let passo = window.document.getElementById('passo')
+    let res = window.document.getElementById('res')
     
+    if (inicio.length == 0 || fim.length == 0  || passo.length == 0 ) {
+        res.innerHTML = 'Impossivel contar'
+    } else {
+        let i = Number(inicio.value)
+        let f = Number(fim.value)
+        let p = Number(passo.value)
+        res.innerHTML = ' '
+
+        if (p <= 0) {
+            window.alert('considerando PASSO = 1')
+            p = 1
+        }
+        if (i < f) {
+            //contegem crescente
+            for (let c = i; c <= f; c += p) {
+                res.innerHTML += `${c} 👉`
+            }
+        } else {
+            //contagem regressiva
+            for (let c = i; c >= f; c -= p) {
+                res.innerHTML += `${c} 👉`
+            }
+        }
+    res.innerHTML += ' ✅' 
+    }
 }
