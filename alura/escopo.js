c3adf002299654b5405bb6b32d4f8fce68c5f63b
Guilherme Duarte <gduartedasilva3@gmail.com>
2026-04-09 13:16:18 -0300

    .

---
+++ alura/escopo.js
@@ -0,0 +1,8 @@
+function cumprimentar() {
+    const nome = 'Gui'
+    const cumprimento = 'Olá'
+    console.log(cumprimento, nome)
+}
+
+cumprimentar()
+console.log(cumprimento, nome)
---
+++ alura/variaveis.js
@@ -0,0 +1,4 @@
+const estudante = 'Guilherme'
+let professora = 'Ana'
+
+console.log(estudante, professora)
