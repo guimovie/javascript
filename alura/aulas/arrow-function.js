058a0aa683a0aaf411bc19a816425c0bda30a3e6
Guilherme Duarte <gduartedasilva3@gmail.com>
2026-04-27 18:05:40 -0300

    Add arrow function examples and greeting exercise
    Add two new JS files demonstrating function and arrow-function usage. alura/aulas/arrow-function.js shows a traditional function, an arrow function with block return, and an implicit-return arrow function with console logs. alura/exercicios/ex-006.js adds a greeting function and an equivalent arrow-version with example logs for exercise purposes.

---
+++ alura/aulas/arrow-function.js
@@ -0,0 +1,15 @@
+const Nome = function (nome) {
+    return nome
+}
+
+const exibeNome = (nome) => {
+    return nome
+}
+
+// OU
+
+const exibeOutroNome = (outroNome) => outroNome
+
+console.log(Nome('Guilherme'))
+console.log(exibeNome('Peterson'))
+console.log(exibeOutroNome('Rodrigo'))
---
+++ alura/exercicios/ex-006.js
@@ -0,0 +1,11 @@
+function saudacao(nome) {
+    return `Olá ${nome}, bem vindo(a)`
+} 
+
+console.log(saudacao('Guilherme'))
+
+// ou
+
+const saudacaoNome = (nome) => `Olá ${nome}, tamo junto`
+
+console.log(saudacaoNome('Guilherme'))
