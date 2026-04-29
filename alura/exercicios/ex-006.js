fd9e12718158dc44303a0da87d097698f1bd767a
Guilherme Duarte <gduartedasilva3@gmail.com>
2026-04-29 18:25:00 -0300

    Update ex-006.js

--- alura/exercicios/ex-006.js
+++ alura/exercicios/ex-006.js
@@ -9,3 +9,25 @@ console.log(saudacao('Guilherme'))
 const saudacaoNome = (nome) => `Olá ${nome}, tamo junto`
 
 console.log(saudacaoNome('Guilherme'))
+
+
+function maiorDeIdade(idade) {
+    if (idade < 18) {
+        console.log(`Sua idade é ${idade}, Menor de Idade`)
+    } else {
+        console.log(`Sua idade é ${idade}, Maior de Idade`)
+    }
+}
+
+maiorDeIdade(23)
+
+function palindromo(palavra) {
+    const palavraInvertida = palavra.split('').reverse().join('')
+    if (palavra.toLowerCase() === palavraInvertida.toLowerCase()) {
+        return `${palavra} é um Palíndromo`
+    } else {
+        return `${palavra} não é um Palíndromo, ao contrário fica ${palavraInvertida}`
+    }
+}
+
+console.log(palindromo('Arara'))
