732be8e0e1a599d13403f71e121a546b2d2c0c7c
Guilherme Duarte <gduartedasilva3@gmail.com>
2026-07-02 20:26:26 -0300

    Create calculando-medias-array.js

---
+++ alura/aulas/calculando-medias-array.js
@@ -0,0 +1,12 @@
+/*
+const nota1 = 10
+const nota2 = 6.5
+const nota3 = 7.5
+const nota4 = 8
+*/
+
+//const media = (nota1 + nota2 + nota3 + nota4) / 4
+
+const nota = [10, 6.5, 7.5, 8]
+const media = (nota[0] + nota[1] + nota[2] + nota[3]) / nota.length
+console.log(`A média das notas é ${media}`)
